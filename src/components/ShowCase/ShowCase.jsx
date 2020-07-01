3e70f1b8557b30aadd0fa7afe0af8e6314605ba4
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-07-01 15:54:25 -0700

    init showTxt component

--- src/components/ShowCase/ShowCase.jsx
+++ src/components/ShowCase/ShowCase.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './ShowCase.module.css'
 import Nav from '../../components/Nav/Nav';
+import ShowTxt from '../../components/ShowTxt/ShowTxt';
 
 const ShowCase = () => {
     return (
         <div className={styles.background}>
             <Nav />
+            <ShowTxt />
         </div>
     )
 };
---
+++ src/components/ShowTxt/ShowTxt.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ShowTxt.module.css';
+
+
+const ShowTxt = () => {
+    return (
+        <div>
+            <h1>The Sky Is The Limit</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    )
+}
+
+export default ShowTxt;
