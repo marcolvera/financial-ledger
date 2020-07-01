72708b5bb5216689a74001a7c49b3fd484e1f88c
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-07-01 16:10:15 -0700

    update showtxt

--- src/components/ShowTxt/ShowTxt.jsx
+++ src/components/ShowTxt/ShowTxt.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './ShowTxt.module.css';
+import { Link } from 'react-router-dom';
 
 
 const ShowTxt = () => {
     return (
-        <div>
-            <h1>The Sky Is The Limit</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className={styles.box}>
+            <h1 className={styles.h1}>The Sky Is The Limit</h1>
+            <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <Link className={styles.btn}>>Read More</Link>
         </div>
     )
 }
