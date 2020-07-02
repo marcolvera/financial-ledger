9699db9395d25a251ee1950031d9521d2c0469a0
Marco Olvera <marcoolvera@MacBook-Pro.frontierlocal.net>
2020-07-02 15:40:02 -0700

    add card image and content

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,21 @@ import styles from './Card.module.css';
 
 const Card = () => {
     return (
-        <div><h1>Hello World</h1></div>
+        <div className={styles.row}>
+            <div className={styles.column}>
+                <div className={styles.column1}>
+                    <img src="https://i.imgur.com/DywFUYq.jpg" alt=""/>
+                </div>
+            </div>
+            <div className={styles.column}>
+                <div className={styles.column2}>
+                    <h4>What are you looking for</h4>
+                    <h2>We provide bespoken solutions</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos omnis 
+                        quas atque quidem deserunt magnam exercitationem possimus dicta velit saepe.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
