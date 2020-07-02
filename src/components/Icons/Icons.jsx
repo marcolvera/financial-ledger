00b73f86844d4e6e01bf1e215f0a5400f6c0cb99
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-07-01 18:17:06 -0700

    init icons component

---
+++ src/components/Icons/Icons.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Icons.module.css';
+
+const Icons = () => {
+    return (
+        <div>
+            <h1>Hell world</h1>
+        </div>
+    )
+}
+
+export default Icons;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ShowCase from '../../components/ShowCase/ShowCase';
+import Icons from '../../components/Icons/Icons';
 
 
 const Home = () => {
     return(
         <div>
            <ShowCase />
+           <Icons />
         </div>
     )
 };
