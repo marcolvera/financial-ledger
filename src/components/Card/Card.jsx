e909dbcca6ccdcb0083dd30c91d60a662f1cb45e
Marco Olvera <marcoolvera@macbook-pro.home>
2020-07-07 10:07:20 -0700

    launch heroku

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -15,7 +15,7 @@ const Card = () => {
                     <h4 className={styles.h4}>What are you looking for</h4>
                     <h2 className={styles.h2}>We provide bespoken solutions</h2>
                     <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perspiciatis 
-                    ea ullam quidem officiis, assumenda sunt ducimus, laudantium quaerat deserunt soluta placeat dolorum, quis mollitia 
+                    ea ullam quidem officiis, assumenda sunt ducimus, laudantiume quaerat deserunt soluta placeat dolorum, quis mollitia 
                     voluptates maiores saepe possimus velit quibusdam. Delectus perspiciatis expedita aliquid.</p>
                     <Link className={styles.btn}>>READ MORE</Link>
                 </div>
