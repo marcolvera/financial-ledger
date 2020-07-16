e6b8e90abe99048055aba7baf007d4d37a067ff8
Marco Olvera <marcoolvera@MacBook-Pro.home>
2020-07-15 19:08:35 -0700

    create post1 comp

---
+++ src/components/Post1/Post1.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Post1.module.css';
+import {Link} from 'react-router-dom';
+
+
+const Card = () => {
+    return (
+        <div className={styles.row}>
+            <div className={styles.column}>
+                <div className={styles.column1}>
+                    <img className={styles.img} src="https://i.imgur.com/kBPSkoh.jpg" alt=""/>
+                </div>
+            </div>
+            <div className={styles.column}>
+                <div className={styles.column2}>
+                    <h2 className={styles.h2}>Blog Post One</h2>
+                    <h4 className={styles.h4}>Posted by Ashley Snook | May 19 2020</h4>
+                    <p className={styles.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perspiciatis 
+                    ea ullam quidem officiis, assumenda sunt ducimus, laudantiume quaerat deserunt soluta placeat dolorum, quis mollitia 
+                    voluptates maiores saepe possimus velit quibusdam. Delectus perspiciatis expedita aliquid.</p>
+                    <Link className={styles.btn}>READ MORE</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
+                    
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav';
+import Post1 from '../../components/Post1/Post1';
+
 
 const Blog = () => {
     return(
         <div>
             <Nav />
+            <Post1 />
         </div>
     );
 }
